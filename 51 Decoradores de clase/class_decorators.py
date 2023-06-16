872d2f3fea9dc9cbc6017545ccf61c26082b4a43
私はダムです <pedrovqz69@gmail.com>
2023-06-16 14:26:19 -0600

    Add repr_decorator function

--- 51 Decoradores de clase/class_decorators.py
+++ 51 Decoradores de clase/class_decorators.py
@@ -4,9 +4,29 @@
 # Es similar a los decoradores de funciones (es metaprogramación)
 
 
+import inspect
+
+
 def repr_decorator(cls):
     print('Se ejecuta el decorador de la clase')
     print(f'Recibimos el objeto de la clase: {cls.__name__}')
+
+    # Revisamos los atributos de la clase con el método vars
+    attributes = vars(cls)
+    
+    # Iteramos cada atributo
+    # [print(name, attributes) for name, attributes in attributes.items()]
+
+    # Revisamos si se ha sobreescrito el método __init__
+    if '__init__' not in attributes:
+        raise TypeError(f'{cls.__name__} no ha sobreescrito el método __init__')
+    
+    init_sign = inspect.signature(cls.__init__)
+    print(f'Firma del método __init__: {init_sign}')
+
+    # Recuperamos los parámetros, excepto el primero que es self
+    init_parameters  = list(init_sign.parameters)[1:]
+    print(f'Parámetros init: {init_parameters}')
     return cls
 
 
@@ -29,7 +49,5 @@ class Person(object):
     #     return f'Persona({self._name}, {self._lastname})'
 
 
-p1 = Person('Aideé', 'Correa')
-
 if __name__ == '__main__':
-    pass
+    p1 = Person('Aideé', 'Correa')
