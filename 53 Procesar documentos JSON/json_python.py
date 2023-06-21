feebe187b226d816351bb326861473ff9ca1514d
私はダムです <pedrovqz69@gmail.com>
2023-06-21 10:03:14 -0600

    Add json_python.py | Modify url.header()

--- 53 Procesar documentos JSON/json_python.py
+++ 53 Procesar documentos JSON/json_python.py
@@ -1,2 +1,13 @@
 # Leer el archivo JSON (Javascript Object Notation)
 
+from urllib.request import Request, urlopen
+
+url = Request("http://globalmentoring.com.mx/api/personas.json")
+url.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15')
+
+request = urlopen(url)
+
+print(f"Respuesta del servidor:\n{request}")
+
+body = request.read()
+print(f"Cuerpo del documento:\n{body}")
