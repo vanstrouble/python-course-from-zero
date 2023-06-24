bafce15e3d28a45228ac2f801b4c48b4033d9199
私はダムです <pedrovqz69@gmail.com>
2023-06-23 19:14:08 -0600

    Just a simple image .ico to add in the Hello World window | 54 Section

---
+++ 54 TKinter/hello_world_tk.py
@@ -0,0 +1,9 @@
+# GUI
+# TKinter: TK interface
+import tkinter as tk
+
+
+window = tk.Tk()
+window.geometry("1280x720")  # Tamaño de la ventana
+window.title("Hello World Testing TKinter!")  # Nombre de la ventana
+window.mainloop()  # Mostrar la ventana
