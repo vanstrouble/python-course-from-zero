56dbb00fb9bbcd3ebb8be2274b97c5119caaf128
私はダムです <pedrovqz69@gmail.com>
2023-06-23 19:30:46 -0600

    Add new buttton to the original button

--- 54 TKinter/hello_world_tk.py
+++ 54 TKinter/hello_world_tk.py
@@ -6,8 +6,16 @@ from tkinter import ttk  # Themes library for tkinter
 
 window = tk.Tk()
 window.geometry("1280x720")  # Tamaño de la ventana
-window.title("Hello World Testing TKinter!")  # Nombre de la ventana
+window.title("Testing TKinter!")  # Nombre de la ventana
 window.iconbitmap("54 TKinter/icons/py_ico.ico")  # Icono de la ventana
-buttom1 = ttk.Button(window, text="Presioname")  # Crando un boton de la ventana
+
+# Creando un evento
+def clic_event():
+    buttom1.config(text="Tócame más fuerte")
+    print("Presionas muy duro")
+    button2 = ttk.Button(window, text="Nuevo botón")
+    button2.pack()
+
+buttom1 = ttk.Button(window, text="No presiones el botón", command=clic_event)  # Crando un boton de la ventana
 buttom1.pack()  # Colocando el boton en la ventana
 window.mainloop()  # Mostrar la ventana
