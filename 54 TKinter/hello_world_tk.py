8881c28d0aad0de1f3abc897867c71a2edff9a48
私はダムです <pedrovqz69@gmail.com>
2023-06-23 19:23:18 -0600

    Add an icon and a button to the window

--- 54 TKinter/hello_world_tk.py
+++ 54 TKinter/hello_world_tk.py
@@ -1,9 +1,13 @@
 # GUI
 # TKinter: TK interface
-import tkinter as tk
+import tkinter as tk  # Librería de tkinter
+from tkinter import ttk  # Themes library for tkinter
 
 
 window = tk.Tk()
 window.geometry("1280x720")  # Tamaño de la ventana
 window.title("Hello World Testing TKinter!")  # Nombre de la ventana
+window.iconbitmap("54 TKinter/icons/py_ico.ico")  # Icono de la ventana
+buttom1 = ttk.Button(window, text="Presioname")  # Crando un boton de la ventana
+buttom1.pack()  # Colocando el boton en la ventana
 window.mainloop()  # Mostrar la ventana
