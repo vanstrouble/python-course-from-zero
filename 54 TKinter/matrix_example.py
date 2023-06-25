75fc906d6fa5999b2e50028c295bf6c02a6404da
vanstrouble <vanstrouble@proton.me>
2023-06-24 18:03:44 -0600

    Change clic_event function and add more buttons

--- 54 TKinter/matrix_example.py
+++ 54 TKinter/matrix_example.py
@@ -8,17 +8,27 @@ window.title("Manejo de grid")
 window.iconbitmap("54 TKinter/icons/py_icns.icns")
 
 # Métodos de los eventos
-def event1():
-    button1.config(text="Botón 1 presionado")
-
-def event2():
-    button2.config(text="Botón 2 presionado")
+def clic_event(button):
+    if button == 1:
+        button1.config(text=f"Botón {button} presionado")
+    elif button == 2:
+        button2.config(text=f"Botón {button} presionado")
+    elif button == 3:
+        button3.config(text=f"Botón {button} presionado")
+    elif button == 4:
+        button4.config(text=f"Botón {button} presionado")
 
 # Definición de los botones
-button1 = tk.Button(window, text="Botón 1", command=event1)
-button1.grid(row=0, column=0)
+button1 = tk.Button(window, text="Botón 1", command=lambda: clic_event(button=1))
+button1.grid(row=0, column=0, sticky="W")
 
-button2 = tk.Button(window, text="Botón 2", command=event2)
-button2.grid(row=1, column=0)
+button2 = tk.Button(window, text="Botón 2", command=lambda: clic_event(button=2))
+button2.grid(row=1, column=0, sticky="W")
+
+button3 = tk.Button(window, text="Botón 3", command=lambda: clic_event(button=3))
+button3.grid(row=0, column=1, sticky="W")
+
+button4 = tk.Button(window, text="Botón 4", command=lambda: clic_event(button=4))
+button4.grid(row=1, column=1, sticky="W")
 
 window.mainloop()
