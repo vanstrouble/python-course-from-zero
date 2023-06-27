ff38ae1398b9cd980bcdb32f681f66c92ffc35a8
vanstrouble <vanstrouble@proton.me>
2023-06-27 17:07:39 -0600

    Add functions to button1

--- 54 TKinter/matrix_example.py
+++ 54 TKinter/matrix_example.py
@@ -26,15 +26,15 @@ def click_event(button):
 
 # Definición de los botones
 button1 = tk.Button(window, text="Botón 1", command=lambda: click_event(button=1))
-button1.grid(row=0, column=0, sticky="NSWE")
+button1.grid(row=0, column=0, sticky="NSWE",padx=40, pady=30, ipadx=20, ipady=50, columnspan=2, rowspan=2)
 
-button2 = tk.Button(window, text="Botón 2", command=lambda: click_event(button=2))
-button2.grid(row=1, column=0, sticky="NSWE")
+# button2 = tk.Button(window, text="Botón 2", command=lambda: click_event(button=2))
+# button2.grid(row=1, column=0, sticky="NSWE")
 
-button3 = tk.Button(window, text="Botón 3", command=lambda: click_event(button=3))
-button3.grid(row=0, column=1, sticky="NSWE")
+# button3 = tk.Button(window, text="Botón 3", command=lambda: click_event(button=3))
+# button3.grid(row=0, column=1, sticky="NSWE")
 
-button4 = tk.Button(window, text="Botón 4", command=lambda: click_event(button=4))
-button4.grid(row=1, column=1, sticky="NSWE")
+# button4 = tk.Button(window, text="Botón 4", command=lambda: click_event(button=4))
+# button4.grid(row=1, column=1, sticky="NSWE")
 
 window.mainloop()
